401cffe23afae211768c4ffe687d814ecf0536ca
ahmed <ahmed.akakzia@gmail.com>
2021-08-29 11:05:36 +0200

    grid search in sac

---
+++ grid_search.py
@@ -0,0 +1,45 @@
+import os
+
+
+def mkdir_p(dir):
+    '''make a directory (dir) if it doesn't exist'''
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+
+job_directory = "%s/.job" % os.getcwd()
+scratch = os.environ['SCRATCH']
+
+# Make top level directories
+mkdir_p(job_directory)
+
+lrs = [0.003, 0.03, 0.01]
+updates = [100, 50, 30, 200]
+
+for lr in lrs:
+    for u in updates:
+        job_file = os.path.join(job_directory, "grid_{}_{}.slurm".format(lr, u))
+
+        with open(job_file, 'w') as fh:
+            fh.writelines("#!/bin/bash\n")
+            fh.writelines("#SBATCH --account=oke@cpu\n")
+            fh.writelines("#SBATCH --job-name=test\n")
+            fh.writelines("#SBATCH --partition=cpu_p1\n")
+            fh.writelines("#SBATCH --qos=qos_cpu-dev\n")
+            fh.writelines("#SBATCH --output=sac_{}_{}_%j.out\n".format(lr, u))
+            fh.writelines("#SBATCH --error=sac_{}_{}_%j.out\n".format(lr, u))
+            fh.writelines("#SBATCH --time=2:00:00\n")
+            fh.writelines("#SBATCH --nodes=1\n")
+            fh.writelines("#SBATCH --ntasks=1\n")
+            fh.writelines("#SBATCH --hint=nomultithread\n")
+
+            fh.writelines("module load pytorch-cpu/py3/1.4.0\n")
+
+            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+            fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+            fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
+            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
+
+            fh.writelines("srun python -u -B main.py --agent 'SAC' --lr {} --updates-per-step {} --save-dir 'experiments' 2>&1".format(lr, u))
+
+        os.system("sbatch %s" % job_file)
--- sac_implementation/arguments.py
+++ sac_implementation/arguments.py
@@ -41,11 +41,11 @@ def get_args():
     parser.add_argument('--depth', type=int, default=2, metavar='G',
                         help='Number of discount factors to be used')
 
-    parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=256, metavar='N',
                         help='batch size (default: 256)')
     parser.add_argument('--max-episode-steps', type=int, default=100, metavar='N',
                         help='number of steps per episode (default: 100)')
-    parser.add_argument('--num_steps', type=int, default=200001, metavar='N',
+    parser.add_argument('--num-steps', type=int, default=200001, metavar='N',
                         help='maximum number of steps (default: 1000000)')
     parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                         help='hidden size (default: 256)')
@@ -53,18 +53,18 @@ def get_args():
     parser.add_argument('--update-frequency', type=int, default=1, metavar='N',
                         help='Frequency update between gamma 1 and 2')
 
-    parser.add_argument('--updates_per_step', type=int, default=100, metavar='N',
+    parser.add_argument('--updates-per-step', type=int, default=100, metavar='N',
                         help='model updates per simulator step (default: 1)')
-    parser.add_argument('--start_steps', type=int, default=-1, metavar='N',
+    parser.add_argument('--start-steps', type=int, default=-1, metavar='N',
                         help='Steps sampling random actions (default: 10000)')
-    parser.add_argument('--target_update_interval', type=int, default=2, metavar='N',
+    parser.add_argument('--target-update-interval', type=int, default=2, metavar='N',
                         help='Value target update per no. of updates per step (default: 1)')
-    parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+    parser.add_argument('--replay-size', type=int, default=1000000, metavar='N',
                         help='size of replay buffer (default: 10000000)')
     parser.add_argument('--cuda', action="store_true",
                         help='run on CUDA (default: False)')
 
-    parser.add_argument('--save_interval', type=int, default=10, metavar='N',
+    parser.add_argument('--save-interval', type=int, default=10, metavar='N',
                         help='save and test interval(default: 10)')
 
     parser.add_argument('--save-dir', default="experiments",
